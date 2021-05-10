6268afef9e302f41ce6071acf11b362e419ab480
samuele <marketing@aricasa.com>
2021-05-10 15:45:45 +0200

    initial

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,5 +1,5 @@
 import { createClient } from 'contentful'
-import Image from 'next/Image'
+import Image from 'next/image'
 /** il campo Method è un richtext e richiede un componente esterno per essere renderizzato */
 /** si installa con npm install @contentful/rich-text-react-renderer */
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
